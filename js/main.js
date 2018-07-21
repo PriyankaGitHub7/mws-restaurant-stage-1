52bb6d54366e2da0a9826d26e27787a4c5ea5fc1
PriyankaGitHub7 <madirajupriyanka@gmail.com>
2018-07-21 14:26:22 +0530

    Initial set of UI changes

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoicHJpeWFua2E4OCIsImEiOiJjampiMTlxMjkzbWI5M2twMWN6NXFzcWduIn0.u72_ujygixhyXRwWNaNnEA',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -157,6 +157,8 @@ fillRestaurantsHTML = (restaurants = self.restaurants) => {
  */
 createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
+
+  // li.className = 'restaurant-single';
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
